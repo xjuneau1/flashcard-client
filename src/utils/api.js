8e9ef2880734a5600bb64e92fb5b372a92f2e803
Xander Juneau <xanderjuneau@gmail.com>
2022-11-30 13:03:12 -0500

    changed BASE_URL definition to include VITE_ per Vite documentation

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = import.meta.env.BASE_URL || "http://localhost:5000"
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000"
 
 const headers = new Headers()
 headers.append("Content-Type", "application/json")
