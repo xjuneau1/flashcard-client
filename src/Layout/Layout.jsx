178e390567366ecc57120c528ff697a0e5f79355
Xander Juneau <xanderjuneau@gmail.com>
2022-11-30 16:06:24 -0500

    resize header text for mobile

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,23 +1,33 @@
 import React, { useState } from "react";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
 import Deck from "./Deck/Deck";
 import CreateDeck from "./Deck/CreateDeck";
 import Breadcrumb from "./Breadcrumb";
-import layout from "./layout.module.css"
+import layout from "./layout.module.css";
 function Layout() {
   const [decks, setDecks] = useState([]);
-  const [deck, setDeck] = useState({})
-  const [error, setError] = useState(null)
-  const [pageName, setPageName] = useState("")
+  const [deck, setDeck] = useState({});
+  const [error, setError] = useState(null);
+  const [pageName, setPageName] = useState("");
   return (
     <div className={layout["layout-container"]}>
-      <Breadcrumb deck={deck} pageName={pageName} setPageName={setPageName}/>
+      <Breadcrumb deck={deck} pageName={pageName} setPageName={setPageName} />
       <Routes>
-      <Route path="/" element={<Home decks={decks} setDecks={setDecks} />} />
-      <Route path="/decks/new" element={<CreateDeck decks={decks} />} />
-      <Route path="/decks/:deck_id/*" element={<Deck deck={deck} setDeck={setDeck} error={error} setError={setError}/>} />
-    </Routes>
+        <Route path="/" element={<Home decks={decks} setDecks={setDecks} />} />
+        <Route path="/decks/new" element={<CreateDeck decks={decks} />} />
+        <Route
+          path="/decks/:deck_id/*"
+          element={
+            <Deck
+              deck={deck}
+              setDeck={setDeck}
+              error={error}
+              setError={setError}
+            />
+          }
+        />
+      </Routes>
     </div>
   );
 }
